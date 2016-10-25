3fd6e045ac686c3e4d47059c12c31eef478fabf2
lyz <327334883@qq.com>
2016-10-25 11:59:04 +0800

    add TJunction2 for volume

---
+++ source/tjunction2.h
@@ -0,0 +1,68 @@
+/*
+T-SPLINE -- A T-spline object oriented library in C++
+Copyright (C) 2015-  Wenlei Xiao
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2.1 of the
+License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+Report problems and direct all questions to:
+
+Wenlei Xiao, Ph.D
+School of Mechanical Engineering and Automation
+Beijing University of Aeronautics and Astronautics
+D-315, New Main Building, 
+Beijing, P.R. China, 100191
+
+email: xiaowenlei@buaa.edu.cn
+-------------------------------------------------------------------------------
+Revision_history:
+
+2015/04/08: Wenlei Xiao
+- Created.
+2016/04/02: Wenlei Xiao
+- Doxygen comments added.
+-------------------------------------------------------------------------------
+*/
+
+#ifndef TJUNCTION2_H
+#define TJUNCTION2_H
+
+#include <tspline2.h>
+
+#ifdef use_namespace
+namespace TSPLINE {
+	using namespace NEWMAT;
+#endif
+
+DECLARE_SMARTPTR(TJunction2);
+
+/**  
+  *  @class  <TJunction2> 
+  *  @brief  T-junction2 class.
+  *  @note  
+  *  TJunction2 requires a T-vertex2 to be set, it will then derive the shape of the T-junction2 and patch necessary virtual T-objects.
+*/
+class TJunction2
+{
+public:
+	TJunction2(const TVertex2Ptr &vertex);
+	~TJunction2();
+};
+
+#ifdef use_namespace
+}
+#endif
+
+#endif
